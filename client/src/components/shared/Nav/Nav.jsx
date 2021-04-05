56f79e2a57a1cb5c1754ab032f198ae8c495d4cb
Andrew Bayer <arbayer@iu.edu>
2021-04-05 15:23:08 -0400

    quick clean up of console logs etc

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -4,19 +4,19 @@ import "./Nav.css";
 function Nav(props) {
   const { user, handleLogout } = props;
 
-  const authenticatedOptions = (
-    <div className="right">
-      <Link className="right-links" to="/">
-        My Projects
-      </Link>
-      <Link className="right-links" to="/add-project">
-        Add Project
-      </Link>
-      <Link className="right-links" onClick={handleLogout}>
-        Logout
-      </Link>
-    </div>
-  );
+  // const authenticatedOptions = (
+  //   <div className="right">
+  //     <Link className="right-links" to="/">
+  //       My Projects
+  //     </Link>
+  //     <Link className="right-links" to="/add-project">
+  //       Add Project
+  //     </Link>
+  //     <Link className="right-links" onClick={handleLogout}>
+  //       Logout
+  //     </Link>
+  //   </div>
+  // );
   const unauthenticatedOptions = (
     <Link className="right-links" to="/login">
       Login
