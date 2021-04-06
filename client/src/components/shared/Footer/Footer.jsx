28e96ad6689823c44ccb65a5a41f3ed91e3831dd
Andrew Bayer <arbayer@iu.edu>
2021-04-06 09:39:20 -0400

    added footer

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -2,7 +2,30 @@ import "./Footer.css";
 import React from "react";
 
 function Footer(props) {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/andy-bayer-203/"
+      >
+        <img
+          className="social-links"
+          src="https://i.imgur.com/0WzBM5Y.png"
+          alt="linked-in"
+        />
+      </a>
+      <a target="_blank" rel="noreferrer" href="https://github.com/arbayer4">
+        {" "}
+        <img
+          className="social-links"
+          src="https://i.imgur.com/hCulubQ.png"
+          alt="github"
+        />
+      </a>
+      <p>© Andy Bayer 2021</p>
+    </footer>
+  );
 }
 
 export default Footer;
